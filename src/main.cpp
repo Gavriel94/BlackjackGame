660f7604693ad3dae33c21fa1b7e24dbf5120ca5
Anthony <anthonygavriel@gmail.com>
2022-01-29 01:00:00 +0000

    basic gameplay setup, needs testing

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,44 @@
 #include "../include/Player.h"
 #include "../include/Dealer.h"
 
-
 int main() {
+    //instantiating objects to start game
     Deck cards;
     Dealer dealer;
-    dealer.shuffleCards(cards);
+    Player b("Bob");
+    Player j("Jane");
+    std::vector<Player> players;
 
+    //basic gameplay
+    dealer.shuffleCards(cards);
+    dealer.startGame(cards, players);
+    for(auto  player : players) {
+        while(player.getPlaying()) {
+            bool c;
+            c = dealer.hitOrStick(player);
+            if(c) {
+                dealer.hit(cards, player);
+            }
+            else {
+                player.stick();
+            }
+        }
+    }
     while(dealer.inGame()) {
         dealer.hit(cards);
-        dealer.viewCards();
         if(dealer.getValue() > 21) {
-            std::cout << "Dealer bust!";
             dealer.stick();
+        } else if(dealer.getValue() == 21) {
+            std::cout << "Dealer wins!";
+            dealer.stick();
+        }
+        else if(dealer.getValue() == 17) {
+            dealer.stick();
+        }
+    }
+    for(auto player : players) {
+        if(player.getValue() > dealer.getValue() && !player.getBust()) {
+            std::cout << player.getName() << " wins!";
         }
     }
 
