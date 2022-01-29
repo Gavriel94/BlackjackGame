4f55ee4ba2fb494d6760caca40fc56975be171f2
Anthony <anthonygavriel@gmail.com>
2022-01-29 00:59:25 +0000

    Player class added, testing required

--- include/Player.h
+++ include/Player.h
@@ -1,7 +1,3 @@
-//
-// Created by Anthony Gavriel on 27/01/2022.
-//
-
 #ifndef BLACKJACKGAME_PLAYER_H
 #define BLACKJACKGAME_PLAYER_H
 
@@ -11,7 +7,22 @@
 
 class Player {
 public:
+    Player(std::string name);
+    bool hit();
     std::vector<Card> hand;
+    bool stick();
+    int getValue();
+    void viewCards();
+    std::string getName();
+    bool getPlaying();
+    bool getBust();
+    //void viewTable();
+private:
+    int ace();
+    std::string name;
+    bool playing;
+    int value;
+    bool bust;
 };
 
 
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,5 +1,86 @@
-//
-// Created by Anthony Gavriel on 27/01/2022.
-//
-
 #include "../include/Player.h"
+#include "../include/Dealer.h"
+
+#include <utility>
+
+Player::Player(std::string name) {
+    this->name = std::move(name);
+    playing = true;
+    value = 0;
+    bust = false;
+}
+
+//hit is true
+//stick is false
+//knocks player out the game
+//wrap in loop in main
+bool Player::hit() {
+    return playing;
+}
+
+bool Player::stick() {
+    playing = false;
+    return playing;
+}
+
+int Player::getValue() {
+    for(auto i : hand) {
+        if(i.getValue() == "Jack" || i.getValue() == "Queen" ||
+           i.getValue() == "King") {
+            value += 10;
+        } else if(i.getValue() == "Ace") {
+            value += ace();
+        }
+        else {
+            value += std::stoi(i.getValue());
+        }
+    }
+    //keep an eye on this
+    if(value > 21) {
+        std::cout << "Player bust!";
+        bust = true;
+        playing = false;
+    }
+    return value;
+}
+
+int Player::ace() {
+    std::cout << "Would you like the ace to be 1 or 11?" << std::endl;
+    int choice = 0;
+    std::cin >> choice;
+    switch(choice) {
+        case 1:
+            choice = 1;
+            break;
+        case 2:
+            choice = 11;
+            break;
+        default:
+            std::cout << "Error, please press 1 or 11\n";
+            std::cin >> choice;
+    }
+    for(int i = 0; i < 50; i++) {
+        std::cout << "\n";
+    }
+    return choice;
+}
+
+void Player::viewCards() {
+    for(auto i : hand) {
+        std::cout << i;
+    }
+    std::cout << "Total value: " << getValue();
+}
+
+bool Player::getPlaying() {
+    return playing;
+}
+
+std::string Player::getName() {
+    return std::string();
+}
+
+bool Player::getBust() {
+    return bust;
+}
+
